dc391dc1e228befe9062822d58192e99a9ff99b4
Kautism <darkautism@gmail.com>
2020-04-17 15:22:40 +0800

    First sample can upload firmware

---
+++ builder/frameworks/arduino.py
@@ -0,0 +1,129 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Arduino
+
+Arduino Wiring-based Framework allows writing cross-platform software to
+control devices attached to a wide range of Arduino boards to create all
+kinds of creative coding, interactive objects, spaces or physical experiences.
+
+http://arduino.cc/en/Reference/HomePage
+"""
+
+from os.path import isdir, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+board = env.BoardConfig()
+build_core = board.get("build.core", "lgt8f")
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-lgt8fx")
+
+assert isdir(FRAMEWORK_DIR)
+
+CPPDEFINES = [
+    ("F_CPU", "$BOARD_F_CPU"),
+    "ARDUINO_ARCH_AVR",
+    "ARDUINO_ARCH_LGT",
+    ("ARDUINO", 10808)
+]
+
+if "build.usb_product" in board:
+    CPPDEFINES += [
+        ("USB_VID", board.get("build.hwids")[0][0]),
+        ("USB_PID", board.get("build.hwids")[0][1]),
+        ("USB_PRODUCT", '\\"%s\\"' %
+         board.get("build.usb_product", "").replace('"', "")),
+        ("USB_MANUFACTURER", '\\"%s\\"' %
+         board.get("vendor", "").replace('"', ""))
+    ]
+
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CFLAGS=[
+        "-std=gnu11",
+        "-fno-fat-lto-objects"
+    ],
+
+    CCFLAGS=[
+        "-Os",  # optimize for size
+        "-Wall",  # show warnings
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-flto",
+        "-mmcu=$BOARD_MCU"
+    ],
+
+    CXXFLAGS=[
+        "-fno-exceptions",
+        "-fno-threadsafe-statics",
+        "-fpermissive",
+        "-std=gnu++11"
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-mmcu=$BOARD_MCU",
+        "-Wl,--gc-sections",
+        "-flto",
+        "-fuse-linker-plugin"
+    ],
+
+    CPPDEFINES=CPPDEFINES,
+
+    LIBS=["m"],
+
+    LIBSOURCE_DIRS=[
+        join(FRAMEWORK_DIR, "libraries")
+    ],
+
+    CPPPATH=[
+        join(FRAMEWORK_DIR, "cores", build_core)
+    ]
+)
+
+# copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
+env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
+
+#
+# Target: Build Core Library
+#
+
+libs = []
+
+if "build.variant" in board:
+    variants_dir = join(
+        "$PROJECT_DIR", board.get("build.variants_dir")) if board.get(
+            "build.variants_dir", "") else join(FRAMEWORK_DIR, "variants")
+
+    env.Append(
+        CPPPATH=[
+            join(variants_dir, board.get("build.variant"))
+        ]
+    )
+    libs.append(env.BuildLibrary(
+        join("$BUILD_DIR", "FrameworkArduinoVariant"),
+        join(variants_dir, board.get("build.variant"))
+    ))
+
+libs.append(env.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkArduino"),
+    join(FRAMEWORK_DIR, "cores", build_core)
+))
+
+env.Prepend(LIBS=libs)
--- builder/main.py
+++ builder/main.py
@@ -1,72 +1,236 @@
-"""
-    Build script for test.py
-    test-builder.py
-"""
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from os.path import join
-from SCons.Script import AlwaysBuild, Builder, Default, DefaultEnvironment
+from time import sleep
+
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Builder, Default, DefaultEnvironment)
+
+from platformio.util import get_serial_ports
+
+
+def BeforeUpload(target, source, env):  # pylint: disable=W0613,W0621
+    upload_options = {}
+    if "BOARD" in env:
+        upload_options = env.BoardConfig().get("upload", {})
+
+    # Deprecated: compatibility with old projects. Use `program` instead
+    if "usb" in env.subst("$UPLOAD_PROTOCOL"):
+        upload_options['require_upload_port'] = False
+        env.Replace(UPLOAD_SPEED=None)
+
+    if env.subst("$UPLOAD_SPEED"):
+        env.Append(UPLOADERFLAGS=["-b", "$UPLOAD_SPEED"])
+
+    # extra upload flags
+    if "extra_flags" in upload_options:
+        env.Append(UPLOADERFLAGS=upload_options.get("extra_flags"))
+
+    # disable erasing by default
+    env.Append(UPLOADERFLAGS=["-D"])
+
+    if upload_options and not upload_options.get("require_upload_port", False):
+        return
+
+    env.AutodetectUploadPort()
+    env.Append(UPLOADERFLAGS=["-P", '"$UPLOAD_PORT"'])
+
+    if env.subst("$BOARD") in ("raspduino", "emonpi", "sleepypi"):
+
+        def _rpi_sysgpio(path, value):
+            with open(path, "w") as f:
+                f.write(str(value))
+
+        if env.subst("$BOARD") == "raspduino":
+            pin_num = 18
+        elif env.subst("$BOARD") == "sleepypi":
+            pin_num = 22
+        else:
+            pin_num = 4
+
+        _rpi_sysgpio("/sys/class/gpio/export", pin_num)
+        _rpi_sysgpio("/sys/class/gpio/gpio%d/direction" % pin_num, "out")
+        _rpi_sysgpio("/sys/class/gpio/gpio%d/value" % pin_num, 1)
+        sleep(0.1)
+        _rpi_sysgpio("/sys/class/gpio/gpio%d/value" % pin_num, 0)
+        _rpi_sysgpio("/sys/class/gpio/unexport", pin_num)
+    else:
+        if (not upload_options.get("disable_flushing", False)
+                and not env.get("UPLOAD_PORT", "").startswith("net:")):
+            env.FlushSerialBuffer("$UPLOAD_PORT")
+
+        before_ports = get_serial_ports()
+
+        if upload_options.get("use_1200bps_touch", False):
+            env.TouchSerialPort("$UPLOAD_PORT", 1200)
+
+        if upload_options.get("wait_for_upload_port", False):
+            env.Replace(UPLOAD_PORT=env.WaitForNewSerialPort(before_ports))
+
 
 env = DefaultEnvironment()
 
-# A full list with the available variables
-# http://www.scons.org/doc/production/HTML/scons-user.html#app-variables
 env.Replace(
-    AR="ar",
-    AS="gcc",
-    CC="gcc",
-    CXX="g++",
-    OBJCOPY="objcopy",
-    RANLIB="ranlib",
+    AR="avr-gcc-ar",
+    AS="avr-as",
+    CC="avr-gcc",
+    GDB="avr-gdb",
+    CXX="avr-g++",
+    OBJCOPY="avr-objcopy",
+    RANLIB="avr-gcc-ranlib",
+    SIZETOOL="avr-size",
 
-    UPLOADER=join("$PIOPACKAGES_DIR", "tool-bar", "uploader"),
-    UPLOADCMD="$UPLOADER $SOURCES"
+    ARFLAGS=["rc"],
+
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.bootloader)\s+(\d+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
+    SIZEPRINTCMD='$SIZETOOL --mcu=$BOARD_MCU -C -d $SOURCES',
+
+    PROGSUFFIX=".elf"
 )
 
 env.Append(
-    ARFLAGS=["..."],
-
-    ASFLAGS=["flag1", "flag2", "flagN"],
-    CCFLAGS=["flag1", "flag2", "flagN"],
-    CXXFLAGS=["flag1", "flag2", "flagN"],
-    LINKFLAGS=["flag1", "flag2", "flagN"],
-
-    CPPDEFINES=["DEFINE_1", "DEFINE=2", "DEFINE_N"],
-
-    LIBS=["additional", "libs", "here"],
-
     BUILDERS=dict(
-        ElfToBin=Builder(
-            action=" ".join([
+        ElfToEep=Builder(
+            action=env.VerboseAction(" ".join([
                 "$OBJCOPY",
                 "-O",
-                "binary",
+                "ihex",
+                "-j",
+                ".eeprom",
+                '--set-section-flags=.eeprom="alloc,load"',
+                "--no-change-warnings",
+                "--change-section-lma",
+                ".eeprom=0",
                 "$SOURCES",
-                "$TARGET"]),
-            suffix=".bin"
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".eep"
+        ),
+
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
         )
     )
 )
 
-# The source code of "platformio-build-tool" is here
-# https://github.com/platformio/platformio-core/blob/develop/platformio/builder/tools/platformio.py
+# Allow user to override via pre:script
+if env.get("PROGNAME", "program") == "program":
+    env.Replace(PROGNAME="firmware")
 
 #
 # Target: Build executable and linkable firmware
 #
-target_elf = env.BuildProgram()
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.hex")
+else:
+    target_elf = env.BuildProgram()
+    target_firm = env.ElfToHex(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
 
 #
-# Target: Build the .bin file
+# Target: Print binary size
 #
-target_bin = env.ElfToBin(join("$BUILD_DIR", "firmware"), target_elf)
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
 
 #
-# Target: Upload firmware
+# Target: Upload by default .hex file
 #
-upload = env.Alias(["upload"], target_bin, "$UPLOADCMD")
-AlwaysBuild(upload)
+
+upload_protocol = env.subst("$UPLOAD_PROTOCOL")
+
+if upload_protocol == "micronucleus":
+    env.Replace(
+        UPLOADER="micronucleus",
+        UPLOADERFLAGS=[
+            "-c", "$UPLOAD_PROTOCOL",
+            "--timeout", "60"
+        ],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES"
+    )
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+elif upload_protocol == "custom":
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+else:
+    env.Replace(
+        UPLOADER="avrdude",
+        UPLOADERFLAGS=[
+            "-p", "$BOARD_MCU",
+            "-C",
+            join(env.PioPlatform().get_package_dir("tool-avrdude") or "",
+                 "avrdude.conf"),
+            "-c", "$UPLOAD_PROTOCOL"
+        ],
+        UPLOADCMD='$UPLOADER $UPLOADERFLAGS -U flash:w:$SOURCES:i',
+        UPLOADEEPCMD='$UPLOADER $UPLOADERFLAGS -U eeprom:w:$SOURCES:i'
+    )
+
+    if int(ARGUMENTS.get("PIOVERBOSE", 0)):
+        env.Prepend(UPLOADERFLAGS=["-v"])
+
+    upload_actions = [
+        env.VerboseAction(BeforeUpload, "Looking for upload port..."),
+        env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE $UPLOADCMD")
+    ]
+
+AlwaysBuild(env.Alias("upload", target_firm, upload_actions))
 
 #
-# Target: Define targets
+# Target: Upload EEPROM data (from EEMEM directive)
 #
-Default(target_bin)
+target_uploadeep = env.Alias(
+    "uploadeep",
+    join("$BUILD_DIR", "${PROGNAME}.eep")
+    if "nobuild" in COMMAND_LINE_TARGETS else env.ElfToEep(target_elf), [
+        env.VerboseAction(BeforeUpload, "Looking for upload port..."),
+        env.VerboseAction("$UPLOADEEPCMD", "Uploading $SOURCE")
+    ])
+AlwaysBuild(target_uploadeep)
+
+#
+# Target: Upload firmware using external programmer
+#
+
+target_program = env.Alias(
+    "program", target_firm,
+    env.VerboseAction("$UPLOADCMD", "Programming $SOURCE"))
+AlwaysBuild(target_program)
+
+#
+# Setup default targets
+#
+
+Default([target_buildprog, target_size])
